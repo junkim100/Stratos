22083d29aae42b69919dc76473a791c5cea46164
Jun Kim <junkim100@gmail.com>
2024-11-14 11:27:17 +0900

    Remove all frontend files

--- frontend/src/api/config.ts
+++
@@ -1,6 +0,0 @@
-export const API_CONFIG = {
-    BASE_URL: `http://${window.location.hostname}:51441/api`,  // Note the /api prefix
-    HEADERS: {
-        'Content-Type': 'application/json'
-    }
-};
